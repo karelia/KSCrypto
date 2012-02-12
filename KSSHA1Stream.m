322b8225e3f35fe9c848c90c0ae7504c9af88034
Abizer Nasir <abizern@abizern.org>
2012-02-12 22:44:11 +0000

    Add BSD licence to files and put text in README

--- KSSHA1Stream.m
+++ KSSHA1Stream.m
@@ -5,6 +5,25 @@
 //  Created by Mike on 12/03/2011.
 //  Copyright 2011-2012 Karelia Software. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions are met:
+//      * Redistributions of source code must retain the above copyright
+//        notice, this list of conditions and the following disclaimer.
+//      * Redistributions in binary form must reproduce the above copyright
+//        notice, this list of conditions and the following disclaimer in the
+//        documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//  DISCLAIMED. IN NO EVENT SHALL MIKE ABDULLAH OR KARELIA SOFTWARE BE LIABLE FOR ANY
+//  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 
 #import "KSSHA1Stream.h"
 
@@ -24,7 +43,7 @@
 {
     unsigned char digest[CC_SHA1_DIGEST_LENGTH];
 	CC_SHA1_Final(digest, &_ctx);
-	
+
     _digest = [[NSData alloc] initWithBytes:digest length:CC_SHA1_DIGEST_LENGTH];
 }
 
@@ -56,7 +75,7 @@
     [stream write:[self bytes] maxLength:[self length]];
     [stream close];
     NSData *result = [[[stream SHA1Digest] copy] autorelease];
-    
+
     [stream release];
     return result;
 }
@@ -64,10 +83,10 @@
 - (NSString *)ks_SHA1DigestString
 {
 	static char sHEHexDigits[] = "0123456789abcdef";
-	
+
     NSData *digestData = [self ks_SHA1Digest];
     unsigned char *digest = (unsigned char *)[digestData bytes];
-    
+
 	unsigned char digestString[2 * CC_SHA1_DIGEST_LENGTH];
     NSUInteger i;
 	for (i=0; i<CC_SHA1_DIGEST_LENGTH; i++)
@@ -75,7 +94,7 @@
 		digestString[2*i]   = sHEHexDigits[digest[i] >> 4];
 		digestString[2*i+1] = sHEHexDigits[digest[i] & 0x0f];
 	}
-    
+
 	return [[[NSString alloc] initWithBytes:(const char *)digestString
                                      length:2 * CC_SHA1_DIGEST_LENGTH
                                    encoding:NSASCIIStringEncoding] autorelease];
@@ -93,29 +112,29 @@
 {
     OBPRECONDITION(URL);
     KSSHA1Stream *hasher = [[KSSHA1Stream alloc] initWithURL:URL];
-    
+
     NSData *result;
     if ([URL isFileURL])
     {
         NSInputStream *stream = [[NSInputStream alloc] initWithFileAtPath:[URL path]];
         [stream open];
-        
+
 #define READ_BUFFER_SIZE 64*CC_SHA1_BLOCK_BYTES
         uint8_t buffer[READ_BUFFER_SIZE];
-        
+
         while ([stream streamStatus] < NSStreamStatusAtEnd)
         {
             NSInteger length = [stream read:buffer maxLength:READ_BUFFER_SIZE];
-            
+
             if (length > 0)
             {
                 NSInteger written = [hasher write:buffer maxLength:length];
                 OBASSERT(written == length);
             }
         }
-        
+
         [stream release];
-        
+
         [hasher close];
         result = [hasher SHA1Digest];
     }
@@ -126,12 +145,12 @@
         {
             [[NSRunLoop currentRunLoop] runUntilDate:[NSDate distantPast]];
         }
-        
+
         // Finish up. Empty hash means load failed
         if (![result length]) result = nil;
     }
-    
-    
+
+
     // Finish up. Empty hash means load failed
     result = [[result copy] autorelease];
     [hasher release];
@@ -141,10 +160,10 @@
 - (id)initWithURL:(NSURL *)URL;
 {
     [self init];
-    
-    [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL:URL] 
+
+    [NSURLConnection connectionWithRequest:[NSURLRequest requestWithURL:URL]
                                   delegate:self];
-	
+
     return self;
 }
 
