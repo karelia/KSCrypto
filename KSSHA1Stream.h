362c73a7a51ce9d428490b3aa0f6b3ef9d1991d1
Mike Abdullah <me@mikeabdullah.net>
2013-07-23 10:50:40 +0100

    Document all public methods

--- KSSHA1Stream.h
+++ KSSHA1Stream.h
@@ -36,7 +36,12 @@
     NSData      *_digest;
 }
 
-// nil until you call -close
+/**
+ @result The SHA1 digest of all bytes written to the stream.
+ 
+ The stream needs to know when you've finished writing bytes. This is signified
+ by calling `-close`. So until `-close` is called, this method returns `nil`.
+ */
 @property(nonatomic, copy, readonly) NSData *SHA1Digest;
 
 @end
@@ -44,11 +49,24 @@
 
 @interface KSSHA1Stream (KSURLHashing)
 
+/**
+ Hashes the contents of a file.
+ 
+ @param URL The URL of the file to be hashed. May be anything Cocoa's URL Loading system supports.
+ @result The SHA1 digest of the file at `URL`, or `nil` if accessing the file failed.
+ */
 + (NSData *)SHA1DigestOfContentsOfURL:(NSURL *)URL;
 
-// Only suitable for calling from threads with a running runloop at present
-// Completion handler is called on an arbitrary thread/queue
-// digest is nil if failed to load for some reason, and then error should give some more info
+/**
+ Asynchronously hashes the contents of a file.
+ 
+ Only suitable for calling from threads with a running runloop at present.
+ `handler` is called on an arbitrary thread/queue. `digest` is `nil` if accesing
+ the file failed, with `error` providing details.
+ 
+ @param url The URL of the file to be hashed. May be anything Cocoa's URL Loading system supports.
+ @param handler A block to be called when hashing finishes
+ */
 + (void)SHA1HashContentsOfURL:(NSURL *)url completionHandler:(void (^)(NSData *digest, NSError *error))handler __attribute__((nonnull(1,2)));
 
 @end
@@ -59,10 +77,26 @@
 
 @interface NSData (KSSHA1Stream)
 
-// Cryptographic hashes
+/**
+ Hashes an `NSData` object.
+ 
+ @return The SHA1 digest of the receiver.
+ */
 - (NSData *)ks_SHA1Digest;
 
+/**
+ Hashes an `NSData` object.
+ 
+ @return The SHA1 digest of the receiver, in a human-friendly hex form.
+ */
 - (NSString *)ks_SHA1DigestString;
+
+/**
+ Converts a raw SHA1 digest to a more human-friendly string form.
+ 
+ @param digest An existing SHA1 digest
+ @return The SHA1 digest, converted to a human-friendly hex form.
+ */
 + (NSString *)ks_stringFromSHA1Digest:(NSData *)digest;
 
 @end
